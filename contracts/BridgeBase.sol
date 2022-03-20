358b6334ed2ca5221c2ded5ea3298660e9ab6c05
Constantine <c.stanoykov@yahoo.com>
2022-03-20 18:21:18 +0000

    - contract for IToken created. - constructor for BridgeBase added

--- contracts/BridgeBase.sol
+++ contracts/BridgeBase.sol
@@ -1,8 +1,18 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 import "hardhat/console.sol";
+import './IToken.sol';
 
 contract BridgeBase {
+  address public admin;
+  IToken public token;
+  
+  constructor(address _token){
+    admin = msg.sender;
+    token = IToken(_token);
+  }
 
+// transferFrom(msg.sender, address(this), amount)
 }
--- contracts/RopstenBridge.sol
+++ contracts/RopstenBridge.sol
@@ -1,3 +1,14 @@
+//SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract RopstenBridge {}
+contract RopstenBridge {
+
+  /**
+    1. Sends tokens to the bridge smart contract.
+    1.1 Burns the token.
+    1.2 Event is emitted with the details of the transfer (including recipient address and the amount transfered).
+    1.3 Bridge API receisves the transfer event.
+    1.4 Bridge API sends a transaction to the other Bridge Smart contract.
+    1.5 This Bridge mints the same amount of tokens that was destroyed and send it to the recepient address.  
+   */
+}
