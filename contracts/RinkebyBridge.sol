a661189e676e2e360307423c07c17977a07067bd
Constantine <c.stanoykov@yahoo.com>
2022-04-14 20:16:33 +0100

    - splitSignature and recoverSigner added.

--- contracts/RinkebyBridge.sol
+++ contracts/RinkebyBridge.sol
@@ -37,6 +37,33 @@ contract RinkebyBridge is Ownable{
     nativeToWrapped[nativeTokenAddress] = newWrappedAddress;
   }
 
+  function splitSignature(bytes memory sig) public pure returns (uint8, bytes32, bytes32){
+       require(sig.length == 65);
+       bytes32 r;
+       bytes32 s;
+       uint8 v;
+       assembly {
+           // first 32 bytes, after the length prefix
+           r := mload(add(sig, 32))
+           // second 32 bytes
+           s := mload(add(sig, 64))
+           // final byte (first byte of the next 32 bytes)
+           v := byte(0, mload(add(sig, 96)))
+       }
+     
+       return (v, r, s);
+   }
+
+  function recoverSigner(bytes32 message, bytes memory sig) internal pure returns (address){
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+
+        (v, r, s) = splitSignature(sig);
+
+        return ecrecover(message, v, r, s);
+    }
+
   function tokenExists(address nativeTokenAddress) private view returns (bool exists) /** change to public for testing */ {
     return nativeToWrapped[nativeTokenAddress] != address(0x0);
   }
@@ -52,10 +79,11 @@ contract RinkebyBridge is Ownable{
       emit Transfer(msg.sender, to, amount, block.timestamp);
   }
 
-  function claimToken(address nativeTokenAddress, uint256 amount) public {
+  function claimToken(bytes32 message, bytes memory _signature, address nativeTokenAddress, uint256 amount) public {
       require( amount > 0, 'Trying to claim 0 tokens.');
       require(nativeTokenAddress != address(0x0));
      
+      require(recoverSigner(message, _signature) == msg.sender);
       bool tokenAlreadyExists = tokenExists(nativeTokenAddress);
    
       if(!tokenAlreadyExists){
